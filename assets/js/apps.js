d7c486328355d707e1c5662096a11c7eac90f5fc
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-04-04 16:32:31 -0400

    updated css for html pages

---
+++ assets/js/apps.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('.navbar-toggler').click(function () {
+        $('.navbar-collapse').toggleClass('show');
+    });
+});
+
+const nflwcLink = document.createElement('a');
+const nflwcLogo = document.createElement('img');
+
+nflwcLink.href = 'https://mike-neal.github.io/NFL-Weather-Checker/';
+nflwcLogo.src = './assets/img/nflLogo.jpg';
+nflwcLogo.alt = 'NFL Logo';
+
+nflwcLink.appendChild(nflwcLogo);
+document.getElementById('nflwc').appendChild(nflwcLink);
+
+nflwcLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.open(this.href, '_blank');
+});
+
+const kcmLink = document.createElement('a');
+kcmLink.href = 'https://kcm-app-342e981adbd9.herokuapp.com/';
+kcmLink.textContent = 'Kitchen Cooking and Meal-Planning App';
+document.getElementById('kcm').appendChild(kcmLink).addEventListener('click', function (event) {
+    event.preventDefault();
+    window.open(this.href, '_blank');
+});
+
+const amrapLink = document.createElement('a');
+amrapLink.href = 'https://amrapfitness-vw7b.onrender.com/';
+amrapLink.textContent = 'As Many Reps As Possible';
+document.getElementById('amrap').appendChild(amrapLink).addEventListener('click', function (event) {
+    event.preventDefault();
+    window.open(this.href, '_blank');
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,3 @@ $(document).ready(function () {
     });
 });
 
-const nflwcLink = document.createElement('a');
-nflwcLink.href = 'https://mike-neal.github.io/NFL-Weather-Checker/';
-nflwcLink.textContent = 'NFL Weather Checker';
-document.getElementById('nflwc').appendChild(nflwcLink).addEventListener('click', function (event) {
-    event.preventDefault();
-    window.open(this.href, '_blank');
-});
-
-const kcmLink = document.createElement('a');
-kcmLink.href = 'https://kcm-app-342e981adbd9.herokuapp.com/';
-kcmLink.textContent = 'Kitchen Cooking and Meal-Planning App';
-document.getElementById('kcm').appendChild(kcmLink).addEventListener('click', function (event) {
-    event.preventDefault();
-    window.open(this.href, '_blank');
-});
-
-const amrapLink = document.createElement('a');
-amrapLink.href = 'https://amrapfitness-vw7b.onrender.com/';
-amrapLink.textContent = 'As Many Reps As Possible';
-document.getElementById('amrap').appendChild(amrapLink).addEventListener('click', function (event) {
-    event.preventDefault();
-    window.open(this.href, '_blank');
-});
