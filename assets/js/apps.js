ff445f286aaf87a3de67920ff4f1a63e97c5241f
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-04-04 17:18:16 -0400

    added logos and updated links to apps

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -13,24 +13,35 @@ nflwcLogo.alt = 'NFL Logo';
 
 nflwcLink.appendChild(nflwcLogo);
 document.getElementById('nflwc').appendChild(nflwcLink);
-
 nflwcLink.addEventListener('click', function (event) {
     event.preventDefault();
     window.open(this.href, '_blank');
 });
 
 const kcmLink = document.createElement('a');
+const kcmLogo = document.createElement('img');
+
 kcmLink.href = 'https://kcm-app-342e981adbd9.herokuapp.com/';
-kcmLink.textContent = 'Kitchen Cooking and Meal-Planning App';
-document.getElementById('kcm').appendChild(kcmLink).addEventListener('click', function (event) {
+kcmLogo.src = './assets/img/kcmPic.jpeg';
+kcmLogo.alt = 'KCM Logo';
+
+kcmLink.appendChild(kcmLogo);
+document.getElementById('kcm').appendChild(kcmLink);
+kcmLink.addEventListener('click', function (event) {
     event.preventDefault();
     window.open(this.href, '_blank');
 });
 
 const amrapLink = document.createElement('a');
+const amrapLogo = document.createElement('img');
+
 amrapLink.href = 'https://amrapfitness-vw7b.onrender.com/';
-amrapLink.textContent = 'As Many Reps As Possible';
-document.getElementById('amrap').appendChild(amrapLink).addEventListener('click', function (event) {
+amrapLogo.src = './assets/img/AMRAPLogo.png';
+amrapLogo.alt = 'AMRAP Logo';
+
+amrapLink.appendChild(amrapLogo);
+document.getElementById('amrap').appendChild(amrapLink);
+amrapLink.addEventListener('click', function (event) {
     event.preventDefault();
     window.open(this.href, '_blank');
 });
