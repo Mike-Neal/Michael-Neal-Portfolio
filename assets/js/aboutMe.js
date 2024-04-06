d58c2582d317a18d52dde24601fdade1dea62d1e
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-04-05 22:45:04 -0400

    added resume to about me page linked to profile pic

--- assets/js/aboutMe.js
+++ assets/js/aboutMe.js
@@ -3,3 +3,18 @@ $(document).ready(function () {
         $('.navbar-collapse').toggleClass('show');
     });
 });
+
+const resumeLink = document.createElement('a');
+const resumeLogo = document.createElement('img');
+
+resumeLink.href = './assets/img/MikeNealResume.pdf';
+resumeLogo.src = './assets/img/Profile Pic Baldy.jpg';
+resumeLogo.alt = 'Profile Picture';
+resumeLogo.id = 'profilePic';
+
+resumeLink.appendChild(resumeLogo);
+document.getElementById('profileResume').appendChild(resumeLink);
+resumeLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.open(this.href, '_blank');
+});
