933011e9d807d2bf83f80479055c46abcf3c10db
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-04-08 14:42:15 -0400

    restructured the about me page

--- assets/js/aboutMe.js
+++ assets/js/aboutMe.js
@@ -8,11 +8,14 @@ const resumeLink = document.createElement('a');
 const resumeLogo = document.createElement('img');
 
 resumeLink.href = './assets/img/MikeNealResume.pdf';
+resumeLink.id = 'mnResume';
 resumeLogo.src = './assets/img/Profile Pic Baldy.jpg';
 resumeLogo.alt = 'Profile Picture';
 resumeLogo.id = 'profilePic';
 
 resumeLink.appendChild(resumeLogo);
+resumeLink.style.width = `${resumeLogo.width}px`;
+resumeLink.style.height = `${resumeLogo.height}px`;
 document.getElementById('profileResume').appendChild(resumeLink);
 resumeLink.addEventListener('click', function (event) {
     event.preventDefault();
