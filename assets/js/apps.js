a1a17d519605d8f67f94f8a89fba21b96bfa8bce
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-04-12 17:00:43 -0400

    added apps to the apps page

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -10,6 +10,7 @@ const nflwcLogo = document.createElement('img');
 nflwcLink.href = 'https://mike-neal.github.io/NFL-Weather-Checker/';
 nflwcLogo.src = './assets/img/nflLogo.jpg';
 nflwcLogo.alt = 'NFL Logo';
+nflwcLogo.id = 'nflwcPic';
 
 nflwcLink.appendChild(nflwcLogo);
 document.getElementById('nflwc').appendChild(nflwcLink);
@@ -24,6 +25,7 @@ const kcmLogo = document.createElement('img');
 kcmLink.href = 'https://kcm-app-342e981adbd9.herokuapp.com/';
 kcmLogo.src = './assets/img/kcmPic.jpeg';
 kcmLogo.alt = 'KCM Logo';
+kcmLogo.id = 'kcmPic';
 
 kcmLink.appendChild(kcmLogo);
 document.getElementById('kcm').appendChild(kcmLink);
@@ -38,10 +40,41 @@ const amrapLogo = document.createElement('img');
 amrapLink.href = 'https://amrapfitness-vw7b.onrender.com/';
 amrapLogo.src = './assets/img/AMRAPLogo.png';
 amrapLogo.alt = 'AMRAP Logo';
+amrapLogo.id = 'amrapPic';
 
 amrapLink.appendChild(amrapLogo);
 document.getElementById('amrap').appendChild(amrapLink);
 amrapLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.open(this.href, '_blank');
+});
+
+const pwgrLink = document.createElement('a');
+const pwgrLogo = document.createElement('img');
+
+pwgrLink.href = 'https://mike-neal.github.io/Password-Generator/';
+pwgrLogo.src = './assets/img/pwgrLogo.png';
+pwgrLogo.alt = 'Password Generator Logo';
+pwgrLogo.id = 'pwgrPic';
+
+pwgrLink.appendChild(pwgrLogo);
+document.getElementById('pwgr').appendChild(pwgrLink);
+pwgrLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.open(this.href, '_blank');
+});
+
+const cquizLink = document.createElement('a');
+const cquizLogo = document.createElement('img');
+
+cquizLink.href = 'https://mike-neal.github.io/Coding-Quiz/';
+cquizLogo.src = './assets/img/cquizLogo.jpg';
+cquizLogo.alt = 'Coding Quiz Logo';
+cquizLogo.id = 'cquizPic';
+
+cquizLink.appendChild(cquizLogo);
+document.getElementById('cquiz').appendChild(cquizLink);
+cquizLink.addEventListener('click', function (event) {
     event.preventDefault();
     window.open(this.href, '_blank');
 });
